3ec3816a62d424b4cb65f2673bffb7bceafa5d69
Alex Sneddon <60249426+a-sneddon@users.noreply.github.com>
2024-12-04 11:05:23 +1100

    Rename variables, style updates.

--- get_validated_genomic_recall_precision.py
+++ get_validated_genomic_recall_precision.py
@@ -51,11 +51,11 @@ OPTIONAL.add_argument("--min_stoich",
 parser._action_groups.append(OPTIONAL)
 
 # ARGS = parser.parse_args()
-top_dir = "/home/alex/OneDrive/Projects/m6A_proteins/1_prelim_analysis/1_preprocess_m6A/drs/1_optimise_modkit/from_stefan/"
+top_dir = "/home/alex/OneDrive/Projects/m6A_proteins/1_prelim_analysis/1_preprocess_m6A/drs/1_optimise_modkit"
 ARGS = SimpleNamespace()
-ARGS.validated = f"{top_dir}/VAL_SET_HeLa_glori_control_over10_set.pickle"
-ARGS.input_bed = f"{top_dir}/EXAMPLE_INPUT_HeLa_mRNA_merged.m1.tsv.bugfix2.genomic.site.bed.DRACH"
-ARGS.output = f"{top_dir}/OUTPUT_HeLa_mRNA_merged.m1.tsv.bugfix2.genomic.site.bed.DRACH.recall_vs_precision.tsv"
+ARGS.validated = "./example/validated_genomic_sites.pickle"
+ARGS.input_bed = "./example/predicted_genomic_sites.bed"
+ARGS.output = "./example/test_output.tsv"
 ARGS.aggregate = "avg"
 ARGS.metric = "rate"
 ARGS.discard = None
@@ -65,8 +65,9 @@ ARGS.min_stoich = 0.8
 st = time.time()
 
 validated = ARGS.validated
-m2_path = ARGS.input_bed
+predicted_path = ARGS.input_bed
 out_path = ARGS.output
+
 
 if ARGS.metric not in ["model2","rate"]:
     raise ("--metric must be either model2 or rate")
@@ -84,20 +85,19 @@ with open(validated,"rb") as p:
     validated_set = pickle.load(p)
 
 
-p_lst, all_p_lst = [],[]
+p_lst_val, p_lst_all = [], [] # TODO: Not necessary
 validated_tested = 0
 
 preds_dct_val = {}
 preds_dct_all = {}
 
-
-#parse site level info to and store site thesholds
-with open(m2_path) as f:
+# Parse site level info to and store site thesholds
+with open(predicted_path) as f:
     f.readline()
     for line in f:
         line_lst = line.strip().split("\t")
-        coverage,stoich, prob = line_lst[-3:]
-        contig,start,end = line_lst[:3]
+        coverage, stoich, prob = line_lst[-3:]
+        contig, start, end = line_lst[:3]
         site_index = f"{contig}_{end}"
 
         if ARGS.metric == "rate":
@@ -105,9 +105,9 @@ with open(m2_path) as f:
 
         try:
             if float(stoich) > ARGS.min_stoich:
-                pm2 = float(prob)
+                p_predicted = float(prob)
             else:
-                pm2=0
+                p_predicted = 0
         except:
             print(stoich, prob, "skipped")
             continue
@@ -117,104 +117,106 @@ with open(m2_path) as f:
 
         if ARGS.aggregate == "max":
             if site_index in preds_dct_all:
-                preds_dct_all[site_index] = max(preds_dct_all[site_index], pm2) # store maximum probability if multiple transcripts
+                preds_dct_all[site_index] = max(preds_dct_all[site_index], p_predicted) # store maximum probability if multiple transcripts
             else:
-                preds_dct_all[site_index] = pm2 # store maximum probability if multiple transcripts
+                preds_dct_all[site_index] = p_predicted # store maximum probability if multiple transcripts
             if site_index in validated_set:
                 if site_index in preds_dct_val:    # store maximum probability if multiple transcripts
-                        preds_dct_val[site_index] = max(preds_dct_val[site_index], pm2)
+                        preds_dct_val[site_index] = max(preds_dct_val[site_index], p_predicted)
                 else:
-                    preds_dct_val[site_index] = pm2
+                    preds_dct_val[site_index] = p_predicted
 
         if ARGS.aggregate == "avg":
             coverage = float(coverage)
             if site_index in preds_dct_all:
                 predsum, covsum = preds_dct_all[site_index]
-                predsum += pm2*coverage
-                covsum+=coverage
+                predsum += p_predicted*coverage
+                covsum += coverage
                 preds_dct_all[site_index] = [predsum, covsum] # store avg weighted probability if multiple transcripts
             else:
-                preds_dct_all[site_index] = [pm2*coverage, coverage]
+                preds_dct_all[site_index] = [p_predicted*coverage, coverage]
 
             if site_index in validated_set:
                 if site_index in preds_dct_val:
                     predsum, covsum = preds_dct_val[site_index]
-                    predsum += pm2 * coverage
+                    predsum += p_predicted * coverage
                     covsum += coverage
                     preds_dct_val[site_index] = [predsum, covsum]  # store avg weighted probability if multiple transcripts
                 else:
-                    preds_dct_val[site_index] = [pm2*coverage, coverage]
+                    preds_dct_val[site_index] = [p_predicted*coverage, coverage]
 
 if ARGS.aggregate == "max":
-    all_p_lst = [item for key,item in preds_dct_all.items()]
-    p_lst = [item for key,item in preds_dct_val.items()]
+    p_lst_all = [item for key, item in preds_dct_all.items()]
+    p_lst_val = [item for key, item in preds_dct_val.items()]
 
 if ARGS.aggregate == "avg":
-    all_p_lst = [item[0] / item[1] for key, item in preds_dct_all.items()]
-    p_lst = [item[0] / item[1] for key, item in preds_dct_val.items()]
+    p_lst_all = [item[0] / item[1] for key, item in preds_dct_all.items()]
+    p_lst_val = [item[0] / item[1] for key, item in preds_dct_val.items()]
 
+p_lst_val = sorted(p_lst_val)
+p_lst_all = sorted(p_lst_all)
 
-p_lst = sorted(p_lst)
-all_p_lst = sorted(all_p_lst)
-
-# creates range of thresholds 0.9, 0.901, 0.902 ... 0.989, 0.990, 0.9901 ... 0.9990 ... 0.99999999989 ... 1
-thresholds = [0, 1/10**7, 1/10**6, 1/10**5,1/10**4]+[x/1000 for x in range(1,900)]
+# Initialise thresholds
+thresholds = [0, 1/10**7, 1/10**6, 1/10**5, 1/10**4] + [x/1000 for x in range(1, 900)]
+# Add range of thresholds 0.9, 0.901, 0.902 ... 0.989, 0.990, 0.9901 ... 0.9990 ... 0.99999999989 ... 1
 base_n_lst = [0.9]
-for n1 in range(1,10):
+for n1 in range(1, 10):
     base_n  = str(base_n_lst[-1])
-    for n2 in range(0,9):
-        base_n_lst.append(float(base_n + str(n2)))
-        for n3 in range(0,10):
-            thresholds.append(float(base_n+str(n2)+str(n3)))
+    for n2 in range(0, 9):
+        base_n_to_add = float(base_n + str(n2)) # TODO: String concatenation in this manner is slow
+        base_n_lst.append(base_n_to_add)
+        for n3 in range(0, 10):
+            threshold_to_add = float(base_n + str(n2) + str(n3))
+            thresholds.append(threshold_to_add)
     base_n_lst.append(float(base_n + str(9)))
 thresholds.append(1)
 
 
-#two pointer to get n predicted at thresholds for validated sites
-index_p, index_t = 0,0
+# two pointers to get n predicted at thresholds for validated sites
+# TODO: Just compares number predicted, not whether each individual site is correctly predicted
+index_p, index_t = 0, 0
 out_vals = []
-while index_p < len(p_lst) and index_t < len(thresholds):
-    prob = p_lst[index_p]
+while index_p < len(p_lst_val) and index_t < len(thresholds):
+    prob = p_lst_val[index_p]
     thresh = thresholds[index_t]
     if prob < thresh:
-        index_p+=1
+        index_p += 1
     else:
-        index_t+=1
-        out_vals.append([thresh,1- index_p/len(p_lst),len(p_lst) - index_p])
+        index_t += 1
+        out_vals.append([thresh, 1 - index_p/len(p_lst_val), len(p_lst_val) - index_p])
 
 while index_t < len(thresholds):
     thresh = thresholds[index_t]
-    out_vals.append([thresh, 0,0])
-    index_t+=1
+    out_vals.append([thresh, 0, 0])
+    index_t += 1
 
-#two pointer to get n predicted at thresholds for all sites
-index_p, index_t = 0,0
+# two pointers to get n predicted at thresholds for all sites
+index_p, index_t = 0, 0
 out_vals_all = []
-while index_p < len(all_p_lst) and index_t < len(thresholds):
-    prob = all_p_lst[index_p]
+while index_p < len(p_lst_all) and index_t < len(thresholds):
+    prob = p_lst_all[index_p]
     thresh = thresholds[index_t]
     if prob < thresh:
-        index_p+=1
+        index_p += 1
     else:
-        index_t+=1
-        out_vals_all.append([thresh,1- index_p/len(all_p_lst),len(all_p_lst) - index_p])
+        index_t += 1
+        out_vals_all.append([thresh, 1 - index_p/len(p_lst_all), len(p_lst_all) - index_p])
 
 while index_t < len(thresholds):
     thresh = thresholds[index_t]
-    out_vals_all.append([thresh, 0,0])
-    index_t+=1
+    out_vals_all.append([thresh, 0, 0])
+    index_t += 1
 
-with open(out_path,"w+") as of:
+with open(out_path, "w+") as of:
     of.write("site_threshold\tvalidated_called\tvalidated_rate\tall_called\tall_rate\tvalidated_precision\n")
-    for index,row in enumerate(out_vals):
-        thresh,validated_freq,validated_pred = row
-        thresh,all_freq, all_pred = out_vals_all[index]
-        if all_pred>0:
+    for i, row in enumerate(out_vals):
+        thresh, validated_freq, validated_pred = row
+        thresh, all_freq, all_pred = out_vals_all[i]
+        if all_pred > 0:
             called_validated_percent = validated_pred / all_pred
         else:
-            called_validated_percent=0
-        of.write("\t".join([str(x) for x in [thresh, validated_pred, validated_freq,all_pred,all_freq,called_validated_percent]]) + "\n")
+            called_validated_percent = 0
+        of.write("\t".join([str(x) for x in [thresh, validated_pred, validated_freq, all_pred, all_freq, called_validated_percent]]) + "\n")
 
 
-print("all done in ", time.time() -st, "seconds")
-
+print("all done in ", time.time() - st, "seconds") # TODO: Convert to f-string
