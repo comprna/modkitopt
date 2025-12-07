43ea5ce3da4c9d15d34dc6dc949416ade0e4691f
Alex Sneddon <60249426+a-sneddon@users.noreply.github.com>
2025-12-08 10:35:29 +1100

    Add chunking to t2g to improve memory efficiency.

--- bin/t2g.R
+++ bin/t2g.R
@@ -3,23 +3,17 @@ library(readr)
 library(dplyr)
 library(tibble)
 library(stringr)
-library(magrittr)
 library(glue)
 library(GenomicFeatures)
 
 # Inputs
-args = commandArgs(trailingOnly=TRUE)
+args = commandArgs(trailingOnly = TRUE)
 file_bed <- args[1]
 gff      <- args[2]
 
-# Load modkit output
-file_bed %>%
-  read_tsv() %>%
-  # bedMethyl file header starts with #, so remove from column names
-  rename_with(~ sub("^#", "", .)) %>%
-  # Clean up chrom (transcript ID)
-  mutate(chrom = str_split_i(chrom, "[|]", 1)) ->
-ds
+# Output file
+outfile <- glue("{file_bed}.genomic")
+if (file.exists(outfile)) file.remove(outfile)
 
 # Build the reference database
 txdb  <- makeTxDbFromGFF(gff, format = "gff3")
@@ -27,27 +21,57 @@ txdb  <- makeTxDbFromGFF(gff, format = "gff3")
 # Get all exons by transcript
 exons <- exonsBy(txdb, by = "tx", use.names = TRUE)
 
-# Get the transcriptomic sites
-tx_coords <- GRanges(seqnames = ds$chrom,
-                     ranges = IRanges(start = ds$chromStart, end = ds$chromEnd),
-                     valid_coverage = ds$valid_coverage,
-                     percent_modified = ds$percent_modified)
+# Chunk size for processing bed file memory-efficiently
+chunk_size <- 1e6
 
-# Map transcriptomic sites to genomic sites
-genomic_coords <- mapFromTranscripts(tx_coords, exons)
+# Chunk processing
+process_chunk <- function(df, pos) {
+  message(glue("Processing chunk starting line {pos} with {nrow(df)} rows"))
 
-# Determine if any transcriptomic sites were unmapped
-mapped_indices   <- unique(mcols(genomic_coords)$xHits)
-all_indices      <- seq_along(tx_coords)
-unmapped_indices <- setdiff(all_indices, mapped_indices)
-print(glue("{file_bed} had {length(unmapped_indices)} transcript coordinates unmapped"))
+  df <- df %>%
+    # bedMethyl file header starts with #, so remove from column names
+    rename_with(~ sub("^#", "", .)) %>%
+    # Clean up chrom (transcript ID)
+    mutate(chrom = str_split_i(chrom, "[|]", 1))
 
-# Add metadata to genomic sites
-ds_tx_coords <- as_tibble(as.data.frame(tx_coords)) %>% rowid_to_column("gr_number")
-ds_gen_coords <- as_tibble(as.data.frame(genomic_coords))
+  # Get the transcriptomic sites
+  tx_coords <- GRanges(
+    seqnames = df$chrom,
+    ranges   = IRanges(start = df$chromStart, end = df$chromEnd),
+    valid_coverage   = df$valid_coverage,
+    percent_modified = df$percent_modified
+  )
 
-ds_joined <- left_join(ds_gen_coords,
-                       ds_tx_coords,
-                       by = join_by("xHits" == "gr_number"),
-                       suffix = c("_gen", "_txt"))
-write_tsv(ds_joined, glue("{file_bed}.genomic"))
+  # Map transcriptomic sites to genomic sites
+  genomic_coords <- mapFromTranscripts(tx_coords, exons)
+
+  # Record unmapped sites for this chunk
+  mapped_indices   <- unique(mcols(genomic_coords)$xHits)
+  all_indices      <- seq_along(tx_coords)
+  unmapped_indices <- setdiff(all_indices, mapped_indices)
+  message(glue("Chunk had {length(unmapped_indices)} unmapped coordinates"))
+
+  # Prepare metadata to add to genomic sites
+  df_tx_coords  <- as_tibble(as.data.frame(tx_coords)) %>% rowid_to_column("gr_number")
+  df_gen_coords <- as_tibble(as.data.frame(genomic_coords))
+
+  # Add metadata to genomic sites
+  df_joined <- df_gen_coords %>%
+    left_join(df_tx_coords, by = join_by("xHits" == "gr_number"), suffix = c("_gen", "_txt"))
+
+  # Append to output
+  write_tsv(df_joined, outfile, append = TRUE)
+
+  # Return null since callback has to return something
+  invisible()
+}
+
+# Iterate over chunks
+message("Starting chunked processing of BED data…")
+read_tsv_chunked(
+  file_bed,
+  callback   = SideEffectChunkCallback$new(process_chunk),
+  chunk_size = chunk_size
+)
+
+message("Finished. Output written to: ", outfile)
